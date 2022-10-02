88bed7ccfab24d8683b6bb222147c2a486218491
deb boro <debajit.boro666@gmail.com>
2022-10-01 22:35:14 -0400

    server/schemas added

---
+++ server/seeders/seeds.js
@@ -0,0 +1,75 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { Book, User } = require('../models');
+
+db.once('open', async () => {
+  await Book.deleteMany({});
+  await User.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create friends
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    let friendId = userId;
+
+    while (friendId === userId) {
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      friendId = createdUsers.ops[randomUserIndex];
+    }
+
+    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
+  }
+
+  // create Books
+  let createdBooks = [];
+  for (let i = 0; i < 100; i += 1) {
+    const BookText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+
+    const createdBook = await Book.create({ BookText, username });
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { Books: createdBook._id } }
+    );
+
+    createdBooks.push(createdBook);
+  }
+
+  // create reactions
+  for (let i = 0; i < 100; i += 1) {
+    const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomBookIndex = Math.floor(Math.random() * createdBooks.length);
+    const { _id: BookId } = createdBooks[randomBookIndex];
+
+    await Book.updateOne(
+      { _id: BookId },
+      { $push: { reactions: { reactionBody, username } } },
+      { runValidators: true }
+    );
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
